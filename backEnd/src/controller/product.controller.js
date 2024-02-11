de6d9764be36951fee603b955d28bacfb315c71e
Farzeel <farzeelaftab544@gmail.com>
2024-02-11 22:39:40 +0100

    update and delete product controllers

--- backEnd/src/controller/product.controller.js
+++ backEnd/src/controller/product.controller.js
@@ -3,6 +3,15 @@ import { Product } from "../models/product.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { fileUploadOnCloudonary } from "../utils/cloudinary.js";
+import {v2 as cloudinary} from 'cloudinary';
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+  });
+
 
 const addProduct = async (req, res) => {
   try {
@@ -120,11 +129,11 @@ try {
        existingProduct[key] = updatefields[key]
   }
  })
-
+       
  const updatedProduct = await existingProduct.save();
  return res
  .status(200)
- .json(new ApiResponse(200, updatedProduct));
+ .json(new ApiResponse(200, updatedProduct, "Product Updated Successfully"));
 
   
 } catch (error) {
@@ -132,7 +141,117 @@ try {
   return res.status(500).json(new ApiError(400, "Internal Server Error"));
 }
 
+}
+
+const updateProductPhotos = async(req, res)=>{
+  try {
+    const {productId} = req.params
+  if(!mongoose.Types.ObjectId.isValid(productId)){
+    return res.status(400).json(new ApiError(400, " Invalid id format !"));
+  }
+  const existingProduct = await Product.findById(productId);
+  if (!existingProduct) {
+    return res.status(400).json(new ApiError(400, " Product not found!"));
+  }
+
+
+  const productPhotoLocalPath = req.files?.productPhotos?.map(
+    (file) => file.path
+  );
+
+  if (!productPhotoLocalPath || productPhotoLocalPath?.length === 0) {
+    return res.status(400).json(new ApiError(400, "Photo is Required"));
+  }
+
+  const DeleteUrls =   existingProduct.productImage.map( photo=>{
+    return photo.split('.')[2].slice(39);
+  })
+
+
+    try {
+         await cloudinary.api.delete_resources(DeleteUrls)
+    } catch (error) {
+      console.log("Error WHile Deleting from cloudinary", error)
+    }
+ 
+
+  let cloudinaryUrls = [];
+  await Promise.all(
+    productPhotoLocalPath.map(async (localPath, index) => {
+      try {
+        const cloudinaryUrl = await fileUploadOnCloudonary(localPath);
+        cloudinaryUrls.push(cloudinaryUrl.url);
+      } catch (uploadError) {
+        // Handle the error for the current file but allow other files to continue
+        console.error(
+          `Error uploading file ${index + 1}:`,
+          uploadError.message
+        );
+        cloudinaryUrls.push(null); // or any placeholder value indicating an error
+      }
+    })
+  );
+
+
+  if (!cloudinaryUrls || cloudinaryUrls?.length === 0) {
+    return res.status(400).json(new ApiError(400, "Photo is Required"));
+  }
+
+  // Check if any files failed to upload
+  if (cloudinaryUrls.includes(null)) {
+    return res
+      .status(500)
+      .json(new ApiError(500, "Some files failed to upload"));
+  }
+
+  existingProduct.productImage = cloudinaryUrls
+
+  await existingProduct.save()
+  return res
+  .status(200).
+  json(new ApiResponse(200, existingProduct.productImage, "Photos Updated Successfully"));
+
+    
+  } catch (error) {
+    console.log({catch:error})
+    return res.status(500).json(new ApiError(400, "Internal Server Error"));
+  }
+  
+
 
 }
 
-export { addProduct, getProductDetailsById, updateProduct };
+
+const deleteProduct = async (req, res) => {
+  try {
+    const { productId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(productId)) {
+      return res.status(400).json(new ApiError(400, "Invalid id format!"));
+    }
+
+    const existingProduct = await Product.findById(productId)
+    const DeleteUrls =   existingProduct.productImage.map( photo=>{
+      return photo.split('.')[2].slice(39);
+    })
+      try {
+           await cloudinary.api.delete_resources(DeleteUrls)
+      } catch (error) {
+        console.log("Error WHile Deleting from cloudinary", error)
+      }
+
+    const deletedProduct = await Product.deleteOne({ _id: productId });
+ 
+
+    if (deletedProduct.deletedCount === 0) {
+      return res.status(404).json(new ApiError(404, "Product not found!"));
+    }
+
+    return res.status(200).json(new ApiResponse(200, null, "Product deleted successfully"));
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(new ApiError(500, "Internal Server Error"));
+  }
+};
+
+export { addProduct, getProductDetailsById, updateProduct,updateProductPhotos, deleteProduct };
--- backEnd/src/routes/product.routes.js
+++ backEnd/src/routes/product.routes.js
@@ -1,24 +1,41 @@
-import express from "express"
-import { upload } from "../middlewares/multer.middleware.js"
-import { VerifyJWT } from "../middlewares/auth.middleware.js"
-import { addProduct, getProductDetailsById, updateProduct } from "../controller/product.controller.js"
-import checkIsAdmin from "../middlewares/admin.middleware.js"
+import express from "express";
+import { upload } from "../middlewares/multer.middleware.js";
+import { VerifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  addProduct,
+  deleteProduct,
+  getProductDetailsById,
+  updateProduct,
+  updateProductPhotos,
+} from "../controller/product.controller.js";
+import checkIsAdmin from "../middlewares/admin.middleware.js";
 
+const route = express.Router();
 
-const route  = express.Router()
-
-route.route("/addProduct").post(VerifyJWT,checkIsAdmin, upload.fields([
+route.route("/addProduct").post(
+  VerifyJWT,
+  checkIsAdmin,
+  upload.fields([
     {
-      name:"productPhotos",
-      maxCount:4
-    }
+      name: "productPhotos",
+      maxCount: 4,
+    },
+  ]),
+  addProduct
+);
 
-]) ,addProduct)
+route.route("/productDetails/:productId").get(getProductDetailsById);
+route.route("/updateProduct/:productId").put(VerifyJWT, updateProduct);
+route.route("/updateProductPhoto/:productId").post(
+  VerifyJWT ,
+  upload.fields([
+    {
+      name: "productPhotos",
+      maxCount: 4,
+    },
+  ]),
+  updateProductPhotos
+);
+route.route("/deleteProduct/:productId").delete(VerifyJWT, deleteProduct);
 
-route.route("/productDetails/:productId").get(getProductDetailsById)
-route.route("/updateProduct/:productId").put(updateProduct)
-
-
-
-
-export default route
+export default route;
